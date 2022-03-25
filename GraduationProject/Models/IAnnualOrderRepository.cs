f7daa1c4837bb5d9190ba5693d5892d09e2956fa
ayah karemah <ayaa20002010@hotmail.com>
2022-03-25 17:31:59 +0300

    Order Repository and folders for controllers

---
+++ GraduationProject/Models/IAnnualOrderRepository.cs
@@ -0,0 +1,19 @@
+﻿using GraduationProject.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraduationProject.Models
+{
+    interface IAnnualOrderRepository
+    {
+        public AnnualOrder Create();
+        public IEnumerable<AnnualOrder> GetAnnualOrders();
+        public AnnualOrder GetAnnualOrder();
+        public AnnualOrder Update();
+
+
+
+    }
+}
---
+++ GraduationProject/Models/MockAnnualOrderRepository.cs
@@ -0,0 +1,31 @@
+﻿using GraduationProject.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraduationProject.Models
+{
+    public class MockAnnualOrderRepository : IAnnualOrderRepository
+    {
+        public AnnualOrder Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        public AnnualOrder GetAnnualOrder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<AnnualOrder> GetAnnualOrders()
+        {
+            throw new NotImplementedException();
+        }
+
+        public AnnualOrder Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
