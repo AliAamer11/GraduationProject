ecd750c0d4652a551adaeee9460b3ce919a518d4
ayah karemah <ayaa20002010@hotmail.com>
2022-03-28 21:59:38 +0300

    Add All AnnualOrder CRUD ops

---
+++ GraduationProject/ViewModels/AnnualNeedOrders/EditAnnualNeedOrderViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraduationProject.ViewModels.AnnualNeedOrders
+{
+    public class EditAnnualNeedOrderViewModel
+    {
+        public Data.Models.Items Item { get; set; }
+        public int ItemId { get; set; }
+        public int AnnualOrderID { get; set; }
+        public int FirstSemQuantity { get; set; }
+        public int SecondSemQuantity { get; set; }
+        public int ThirdSemQuantity { get; set; }
+        public int FlowRate { get; set; }
+        public int ApproxRate { get; set; }
+        public int OrderId { get; set; }
+    }
+}
