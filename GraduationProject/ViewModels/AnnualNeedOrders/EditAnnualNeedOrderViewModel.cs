1f5c3d688c4d0e970b2c8620b2e337bd75558b00
ayah karemah <ayaa20002010@hotmail.com>
2022-04-09 20:52:08 +0300

    small edit

--- GraduationProject/ViewModels/AnnualNeedOrders/EditAnnualNeedOrderViewModel.cs
+++ GraduationProject/ViewModels/AnnualNeedOrders/EditAnnualNeedOrderViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,10 +9,18 @@ namespace GraduationProject.ViewModels.AnnualNeedOrders
     public class EditAnnualNeedOrderViewModel
     {
         public Data.Models.Items Item { get; set; }
+
+        [Required(ErrorMessage = "هذا الحقل مطلوب")]
         public int ItemId { get; set; }
         public int AnnualOrderID { get; set; }
+        [Range(0, int.MaxValue, ErrorMessage = "القيمة ليست ضمن المجال المحدد")]
+        [Required(ErrorMessage = "هذا الحقل مطلوب")]
         public int FirstSemQuantity { get; set; }
+        [Range(0, int.MaxValue, ErrorMessage = "القيمة ليست ضمن المجال المحدد")]
+        [Required(ErrorMessage = "هذا الحقل مطلوب")]
         public int SecondSemQuantity { get; set; }
+        [Range(0, int.MaxValue, ErrorMessage = "القيمة ليست ضمن المجال المحدد")]
+        [Required(ErrorMessage = "هذا الحقل مطلوب")]
         public int ThirdSemQuantity { get; set; }
         public int FlowRate { get; set; }
         public string Description { get; set; }
