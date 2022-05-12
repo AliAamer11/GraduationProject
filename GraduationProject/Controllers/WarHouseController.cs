9537356fba1293d6d43f51f627777b5db99b4284
safa marwah <safa.marwah1@hotmail.com>
2022-05-12 16:35:11 +0300

    fixed layouts

--- GraduationProject/Controllers/WarHouseController.cs
+++ GraduationProject/Controllers/WarHouseController.cs
@@ -1,16 +1,60 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using GraduationProject.ViewModels.Items;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspNetCoreHero.ToastNotification.Abstractions;
+using GraduationProject.Data.Models;
+using GraduationProject.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
 
 namespace GraduationProject.Controllers
 {
+    [Authorize(Roles = "VicePris")]
     public class WarHouseController : Controller
     {
+        private readonly INotyfService _notyf;
+        private readonly ApplicationDbContext _context;
+
+        public WarHouseController(ApplicationDbContext context,
+                       INotyfService notyf)
+        {
+            _context = context;
+            _notyf = notyf;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> Items()
+        {
+            var items = await _context.Items.Include(i => i.Category).Include(i => i.Measurement).ToListAsync();
+            foreach (var item in items)
+            {
+                if (item.MinimumRange > item.Quantity)
+                {
+                    _notyf.Error("إن كمية المادة" + " " + item.Name + " " + "ذات الرمز" + " " + item.BarCode + " " + "تخطت الحد الأدنى");
+                }
+            }
+            return View(items);
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> Reports()
+        {
+            var items = await _context.Items.Include(i => i.Category).Include(i => i.Measurement).ToListAsync();
+            foreach (var item in items)
+            {
+                if (item.MinimumRange > item.Quantity)
+                {
+                    _notyf.Error("إن كمية المادة" + " " + item.Name + " " + "ذات الرمز" + " " + item.BarCode + " " + "تخطت الحد الأدنى");
+                }
+            }
+            return View(items);
+
         }
     }
 }
