2e0a5ec1e5e9206a2c5cb5ceeb369d9994874c65
ayah karemah <ayaa20002010@hotmail.com>
2022-05-17 12:36:15 +0300

    package and table with  filtering, sorting and search

--- GraduationProject/Controllers/WarHouseController.cs
+++ GraduationProject/Controllers/WarHouseController.cs
@@ -1,15 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GraduationProject.Data;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Text.Json;
+
+
 
 namespace GraduationProject.Controllers
 {
     public class WarHouseController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public WarHouseController(ApplicationDbContext context)
+        {
+
+
+            _context = context;
+        }
         public IActionResult Index()
         {
+
+            return View();
+        }
+
+        public IActionResult test()
+        {
+            var itemss = _context.Items.ToList();
+            string jsonString = System.Text.Json.JsonSerializer.Serialize(itemss);
+            ViewBag.items = jsonString;
+            //Console.WriteLine(jsonString);
             return View();
         }
     }
