a6442e312ab29baf07762f2dbeeebdd91deb35bc
RamaMarwah <RamaMarwah@github.com>
2022-06-02 12:38:07 +0300

    Items Report Done

--- GraduationProject/Controllers/WarHouseController.cs
+++ GraduationProject/Controllers/WarHouseController.cs
@@ -6,8 +6,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using System.Text.Json;
-
-
+using Microsoft.EntityFrameworkCore;
+using GraduationProject.Data.Models;
+using GraduationProject.ViewModels;
 
 namespace GraduationProject.Controllers
 {
@@ -26,13 +27,49 @@ namespace GraduationProject.Controllers
             return View();
         }
 
+        [HttpGet]
         public IActionResult test()
         {
-            var itemss = _context.Items.ToList();
-            string jsonString = System.Text.Json.JsonSerializer.Serialize(itemss);
+            var allitems = _context.Items.Include(c => c.Category).Include(m => m.Measurement).ToList();
+            List<ItemsDatagridViewModel> itemsDatagrid = new List<ItemsDatagridViewModel>();
+            foreach (var item in allitems)
+            {
+                ItemsDatagridViewModel model = new ItemsDatagridViewModel();
+                model.ItemID = item.ItemID;
+                model.ItemName = item.Name;
+                model.BarCode = item.BarCode;
+                model.Quantity = item.Quantity;
+                model.MinimumRange = item.MinimumRange;
+                model.Status = item.Status;
+                model.Note = item.Note;
+                model.Category = item.Category.Name;
+                model.Measurement = item.Measurement.Name;
+                itemsDatagrid.Add(model);
+            }
+            string jsonString = System.Text.Json.JsonSerializer.Serialize(itemsDatagrid);
             ViewBag.items = jsonString;
-            //Console.WriteLine(jsonString);
             return View();
+
+            //List<Items> model = new List<Items>();
+            //model = allitems;
+            //for (int i = 0; i < allitems.Count; i++)
+            //{
+            //    model[i].ItemID = allitems[i].ItemID;
+            //    model[i].Name = allitems[i].Name;
+            //    model[i].Quantity = allitems[i].Quantity;
+            //    model[i].Status = allitems[i].Status;
+            //    model[i].Note = allitems[i].Note;
+            //    model[i].Measurement.Name = allitems[i].Measurement.Name;
+            //    model[i].Category.Name = allitems[i].Category.Name;
+            //    model.Add(model[i]);
+            //}
+            //string jsonString = System.Text.Json.JsonSerializer.Serialize(model);
+            //ViewBag.items = jsonString;
+
+            //var itemss = _context.Items.ToList();
+            //string jsonString = System.Text.Json.JsonSerializer.Serialize(itemss);
+            //ViewBag.items = jsonString;
+            //Console.WriteLine(jsonString);
         }
     }
 }
---
+++ GraduationProject/ViewModels/ItemsDatagridViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraduationProject.ViewModels
+{
+    public class ItemsDatagridViewModel
+    {
+        public int ItemID { get; set; }
+        public string BarCode { get; set; }
+        public string ItemName { get; set; }
+        public int Quantity { get; set; }
+        public int MinimumRange { get; set; }
+        public string Status { get; set; }
+        public string Note { get; set; }
+        public string Category { get; set; }
+        public string Measurement { get; set; }
+    }
+}
